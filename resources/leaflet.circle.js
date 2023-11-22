b58d46be2666377bbcdcf995ae59e356f1ee2a86
slowe <299787+slowe@users.noreply.github.com>
2023-11-22 12:06:05 +0000

    Add circle help

--- resources/leaflet.circle.js
+++ resources/leaflet.circle.js
@@ -6,6 +6,39 @@
 	var CLICK_EVT = leaflet.Browser.mobile ? 'touchend' : 'click';
 	var ev;
 
+	var PANE_NAME = 'circle';
+
+	var styles = document.createElement('style');
+	styles.innerHTML = '.leaflet-circle-pane.inactive {display:none;} .leaflet-circle-pane { width: 100%; position: absolute; top: unset!important; bottom: 0; left: 0; background: rgb(255,255,255); text-align: center; padding: 0.5em 0.3em; z-index: 2000!important; }';
+	document.head.prepend(styles);
+
+	function insertAfter(newNode, referenceNode) {
+		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	}
+	function createHelp(map, txt) {
+		var pane = map.createPane(PANE_NAME, map.getContainer());
+		pane.classList.add("inactive","fading-enabled");
+
+		var handler = function handler(event) {
+			if (!isTrustedEvent(event)) {
+				return;
+			}
+
+			event.stopPropagation();
+			map.fire('as:point-add', event);
+		};
+
+		pane.addEventListener(CLICK_EVT, handler);
+
+		var panel = leaflet.DomUtil.create('div', 'circle-pane-help');
+		panel.setAttribute('id', 'circle-panel-help');
+		panel.setAttribute('role', 'tooltip');
+		panel.textContent = txt;
+		pane.appendChild(panel);
+
+		return pane;
+	}
+
 	L.Control.circle = L.Control.extend({        
 		options: {
 			position: 'topleft',
@@ -16,11 +49,16 @@
 			this._container.classList.add('open');
 			this._inp.style.display = 'block';
 			this._inp.focus();
+			console.log('activate',this._inp);
 			this._val.style.display = 'block';
 			this._ctl.style.display = 'block';
 			L.DomUtil.addClass(this._map._container,'crosshair-cursor-enabled');
 			this._active = true;
 			this._map.on(CLICK_EVT, ev);
+			
+			var pane = this._map.getPane(PANE_NAME);
+			pane.classList.remove('inactive');
+
 			this.fire('activate');
 		},
 		deactivate: function(keepfocus){
@@ -36,6 +74,10 @@
 			this.options.circle = false;
 			this._active = false;
 			this._map.off(CLICK_EVT, ev);
+
+			var pane = this._map.getPane(PANE_NAME);
+			pane.classList.add('inactive');
+
 			this.fire('deactivate');
 		},
 		setCirclePosition: function(e){
@@ -67,11 +109,12 @@
 			if(typeof ev!=="function") ev = function(e){ _obj.setCirclePosition(e); }
 
 			this._container = L.DomUtil.create('div', 'leaflet-control leaflet-control-circle');
-			this._container.innerHTML = '<div class="leaflet-bar"><form><button class="leaflet-button" title="Create a circle" aria-label="Create a circle"><svg xmlns="http://www.w3.org/2000/svg" overflow="visible" width="16" height="16" fill="currentColor" fill-opacity="0.4" stroke="currentColor" stroke-width="1.5" viewBox="0 0 16 16"><circle cx="8" cy="8" r="7"></circle></svg></button><div class="control" style="display:none;"><input class="radius" id="radius" name="radius" value="'+(this.options.value)+'" type="range" min="1" max="100" /></div><div class="value" style="display:none;"><span></span>km</div></div></form></div>';
+			this._container.innerHTML = '<div class="leaflet-bar"><form><button class="leaflet-button" title="Create a circle" aria-describedby="circle-panel-help" aria-label="Create a circle"><svg xmlns="http://www.w3.org/2000/svg" overflow="visible" width="16" height="16" fill="currentColor" fill-opacity="0.4" stroke="currentColor" stroke-width="1.5" viewBox="0 0 16 16"><circle cx="8" cy="8" r="7"></circle></svg></button><div class="control" style="display:none;"><input class="radius" id="radius" name="radius" value="'+(this.options.value)+'" type="range" min="1" max="100" /></div><div class="value" style="display:none;"><span></span>km</div></div></form></div>';
 			this._btn = this._container.querySelector('button');
 			this._inp = this._container.querySelector('input');
 			this._ctl = this._container.querySelector('.control');
 			this._val = this._container.querySelector('.value');
+			this._hlp = L.DomUtil.create('div','');
 			this._circle = L.circle([0,0],{'radius':1000});
 			this._active = false;
 			
@@ -96,6 +139,8 @@
 				event.preventDefault();
 				event.stopPropagation();
 			});
+			
+			var pane = createHelp(map, (leaflet.Browser.mobile ? "Touch" : "Click on")+" the map to define the centre of the circle then use the slider to change the size.");
 
 			return this._container;
 		}
